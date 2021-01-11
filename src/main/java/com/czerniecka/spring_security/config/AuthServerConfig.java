84928ad5e98931c125ccae132157271257b00fcb
PaulinaCz <paulinaczernieckaa@gmail.com>
2021-01-11 20:37:39 +0000

    token blackboarding

---
+++ src/main/java/com/czerniecka/spring_security/config/AuthServerConfig.java
@@ -0,0 +1,51 @@
+package com.czerniecka.spring_security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("secret")
+                .scopes("read")
+                .authorizedGrantTypes("password", "refresh_token");
+    }
+
+    /*
+    BLACKBOARD - "using same blackboard(DATABASE) to share information"
+    storing token and refresh_token in database
+    when AuthorizationServer & ResourceServer are
+    connecting to the same database
+    * */
+    @Bean
+    public TokenStore tokenStore(){
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore());
+    }
+}
